c89451c2596bfc6c18d6f4455b727036553afedf
Guilherme Lopes Martins <guirockmartins@hotmail.com>
2023-10-28 10:13:08 -0300

    testando controller e view

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -1,15 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("seminariopi3.controller.View", {
             onInit: function () {
+                let default_model = [{
+                    Codigo: "123",
+                    Descricao: "Descrição da descrição"
+                }, {
+                    Codigo: "456",
+                    Descricao: "Descrição número 2"
+                }];
 
+                this.getView().setModel(new JSONModel(default_model), "Default");
             }
         });
     });
