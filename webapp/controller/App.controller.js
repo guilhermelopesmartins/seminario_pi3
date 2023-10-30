b19abc7f92bc2f59b7d12123adc1957a8bcc6f60
Guilherme Lopes Martins <guirockmartins@hotmail.com>
2023-10-30 18:34:43 -0300

    ajustes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,35 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/json/JSONModel"
+],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
       "use strict";
-  
-      return BaseController.extend("seminariopi3.controller.App", {
-        onInit: function() {
-        }
+
+      return Controller.extend("seminariopi3.controller.View", {
+          onInit: function () {
+              let default_model = [{
+                  Codigo: "1",
+                  Descricao: "Aluguel",
+                  Valor: 1000,
+                  Pago: true
+              }];
+
+              this.getView().setModel(new JSONModel(default_model), "Default");
+          },
+          onNewPress: function() {
+            debugger
+            this._oBancosFragment = sap.ui.xmlfragment("sicap.mvc.Bancos.fragments.Bancos", that);
+            this._oBancosFragment.setEscapeHandler((oEscapeHandler) => {
+              this._oBancosFragment.destroy();
+            });
+            this.getView().addDependent(this._oBancosFragment);
+            this._oBancosFragment.addStyleClass(this.getOwnerComponent().getContentDensityClass());
+            //let oModel = this.createRequestModel(CURRENT_MODEL_LIST_API_PATH);
+            //this._oBancosFragment.setModel(oModel)
+            this._oBancosFragment.open()
+          }
       });
-    }
-  );
-  
+  });
